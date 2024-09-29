95db4910e0ddadd8df7086abb8f76d6be6cc8a8d
Raúl Álvarez <91622093+alu0101471136@users.noreply.github.com>
2024-09-29 21:01:49 +0200

    Delete Scripts directory

--- Scripts/ShowLocation.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShowLocation : MonoBehaviour {
-  void Start() {
-    Debug.Log("Posición del objeto con tag " + gameObject.tag + ": " + transform.position);
-  }
-}
